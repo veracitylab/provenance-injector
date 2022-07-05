f2a5ea102a47f80098f69aa04d90ad1741a7d066
Jens Dietrich <jens.dietrich@gmail.com>
2022-07-05 15:12:04 +1200

    switched json encoding to auto-shaded json.org

--- src/main/java/nz/ac/wgtn/veracity/provenance/injector/jee/rt/JSONEncoder.java
+++ src/main/java/nz/ac/wgtn/veracity/provenance/injector/jee/rt/JSONEncoder.java
@@ -1,14 +1,13 @@
 package nz.ac.wgtn.veracity.provenance.injector.jee.rt;
 
-import java.io.ByteArrayOutputStream;
-
-import shadow.com.cedarsoftware.util.io.JsonWriter;
+import org.json.JSONArray;
+import org.json.JSONObject;
 import java.io.PrintWriter;
 import java.util.*;
 import java.util.stream.Collectors;
 
 /**
- * JSON-based encoded. Uses a shadowed version of json.org (to avoid conflicts).
+ * JSON-based encoded. Uses a shaded version of json.org (to avoid conflicts).
  * @author jens dietrich
  */
 public class JSONEncoder implements Encoder {
@@ -19,48 +18,48 @@ public class JSONEncoder implements Encoder {
 
     public void encode(Map<DataKind, List<Object>> data, PrintWriter out) {
 
-//        JSONObject json = new JSONObject();  // check whether we can just use json.put(new JSONObject(data));
-//        for (DataKind kind:data.keySet()) {
-//            String key = kind.name();
-//            List values = data.get(kind);
-//            if (values==null) values = Collections.emptyList();
-//            json.put(key,new JSONArray(values));
-//        }
-//        out.println(json);
-        out.println("{");
-        boolean f1 = true;
+        JSONObject json = new JSONObject();  // check whether we can just use json.put(new JSONObject(data));
         for (DataKind kind:data.keySet()) {
-            if (f1) {
-                f1 = false;
-            }
-            else {
-                out.print(',');
-            }
             String key = kind.name();
-            out.print("\t\"");
-            out.print(key);
-            out.print("\":[");
             List values = data.get(kind);
             if (values==null) values = Collections.emptyList();
-            boolean f2 = true;
-            for (Object value:values) {
-                if (f2) {
-                    f2 = false;
-                }
-                else {
-                    out.print(',');
-                }
-                if (value instanceof Map) {
-                    out.print(flatten(value));
-                } else {
-                    out.print('\"');
-                    out.print(flatten(value));  // assume that we do not have to sanitise this, TODO !
-                    out.print('\"');
-                }
-            }
-            out.print("]");
+            json.put(key,new JSONArray(values));
         }
-        out.println("}");
+        out.println(json);
+//        out.println("{");
+//        boolean f1 = true;
+//        for (DataKind kind:data.keySet()) {
+//            if (f1) {
+//                f1 = false;
+//            }
+//            else {
+//                out.print(',');
+//            }
+//            String key = kind.name();
+//            out.print("\t\"");
+//            out.print(key);
+//            out.print("\":[");
+//            List values = data.get(kind);
+//            if (values==null) values = Collections.emptyList();
+//            boolean f2 = true;
+//            for (Object value:values) {
+//                if (f2) {
+//                    f2 = false;
+//                }
+//                else {
+//                    out.print(',');
+//                }
+//                if (value instanceof Map) {
+//                    out.print(flatten(value));
+//                } else {
+//                    out.print('\"');
+//                    out.print(flatten(value));  // assume that we do not have to sanitise this, TODO !
+//                    out.print('\"');
+//                }
+//            }
+//            out.print("]");
+//        }
+//        out.println("}");
 
     }
 
