f63f1da81973e7e5e49ee74e90c9df2fe4c5c079
Tim White <walton.timothy.james.white@gmail.com>
2024-03-14 17:04:57 +1300

    Add Launcher-Class-Agent and other headers to manifest so -javaagent is unnecessary, cleanup temp dir, more logging

--- provenancify-spring-boot-app.sh
+++ provenancify-spring-boot-app.sh
@@ -9,9 +9,24 @@ APPJARPATH=$(realpath "$1")
 PROVAGENTJARPATH=$(realpath target/provenance-agent.jar)
 EXPLODEDIR=$(mktemp -d)
 
-echo
+echo "APPJARPATH=$APPJARPATH"
+echo "PROVAGENTJARPATH=$PROVAGENTJARPATH"
+echo "EXPLODEDIR=$EXPLODEDIR"
 
 # Explode the provenance agent jar
-( cd "$EXPLODEDIR" && jar xf "$PROVAGENTJARPATH" && jar uf "$APPJARPATH" * )
-
-#rm -r "$EXPLODEDIR"
+(
+  cd "$EXPLODEDIR" &&
+  jar xf "$PROVAGENTJARPATH" &&
+  rm -f META-INF/MANIFEST.MF &&
+  jar xf "$APPJARPATH" META-INF/MANIFEST.MF &&    # jar ...M should make this step unnecessary, but it doesn't
+  jar ufM "$APPJARPATH" * &&
+  (
+    echo "Can-Redefine-Classes: true" &&
+    echo "Can-Retransform-Classes: true" &&
+    echo "Launcher-Agent-Class: nz.ac.wgtn.veracity.provenance.injector.instrument" &&
+    echo " ation.ProvenanceAgent"
+  ) > extra-manifest-entries &&
+  jar ufm "$APPJARPATH" extra-manifest-entries
+) &&
+rm -r "$EXPLODEDIR" &&
+echo DONE
